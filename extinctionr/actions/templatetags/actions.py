af5770ed7c1e21d6976803d80f4568325d5f8892
Dave St.Germain <dstgermain@edx.org>
2019-04-10 16:53:39 -0400

    Add upcoming actions to events page

--- extinctionr/actions/templatetags/actions.py
+++ extinctionr/actions/templatetags/actions.py
@@ -6,6 +6,11 @@ from extinctionr.actions import models
 register = template.Library()
 
 @register.inclusion_tag('recent_actions.html')
-def recent_actions():
-    ract = models.Action.objects.filter(public=True, when__lte=now()).order_by('when')
-    return {'recent': ract}
+def recent_actions(*args, **kwargs):
+    adverb = kwargs.get('adverb', 'recent')
+    ract = models.Action.objects.filter(public=True).order_by('when')
+    if adverb == 'recent':
+        ract = ract.filter(when__lte=now())
+    else:
+        ract = ract.filter(when__gte=now())
+    return {'actions': ract, 'adverb': adverb.capitalize()}
