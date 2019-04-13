5d84bf64a32318904d997314a1bcf65472a758bc
Dave St.Germain <dcs@adullmoment.com>
2019-04-12 23:42:46 -0400

    contact import

--- extinctionr/copy_contacts.py
+++
@@ -1,9 +0,0 @@
-import csv
-import sys
-from contacts.models import Contact
-
-with open(sys.argv[1], 'rb') as fp:
-	reader = csv.reader(fp)
-	for row in reader:
-		print(row)
-
---
+++ extinctionr/info/management/commands/copy_contacts.py
@@ -0,0 +1,34 @@
+from django.core.management.base import BaseCommand, CommandError
+import csv
+from contacts.models import Contact
+
+class Command(BaseCommand):
+    def add_arguments(self, parser):
+        parser.add_argument('filename', type=str)
+
+    def handle(self, *args, **kwargs):
+        first = True
+        with open(kwargs['filename'], 'r') as fp:
+            reader = csv.reader(fp)
+            for row in reader:
+                if first:
+                    first = False
+                    continue
+                name = row[0].split(' ', 1)
+                notes = row[1]
+                email = row[2].lower()
+                phone = row[3].replace('(', '').replace(')', '')
+                group = row[4]
+                if len(name) == 2:
+                    first_name, last_name = name
+                elif len(name) == 1:
+                    first_name = name[0]
+                    last_name = ''
+                else:
+                    first_name = last_name = ''
+                if email:
+                    try:
+                        c = Contact.objects.get(email=email)
+                    except Contact.DoesNotExist:
+                        c = Contact.objects.create(email=email, phone=phone, first_name=first_name, last_name=last_name, description=notes)
+                        self.stdout.write(str(c))
