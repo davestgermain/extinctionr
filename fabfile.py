4e4b2daa4e16b540a193f85f56c5827067946c7a
Dave St.Germain <dcs@adullmoment.com>
2019-04-14 13:50:58 -0400

    Running as service user

--- fabfile.py
+++ fabfile.py
@@ -3,7 +3,7 @@ from fabric import Connection, task
 
 @task
 def deploy(ctx):
-    c = Connection('xrmass.org')
+    c = Connection('xr@xrmass.org')
 
     c.run('git -C /home/src/extinctionr/ pull')
     c.run('supervisorctl restart xr')
--- run.sh
+++ run.sh
@@ -1,8 +1,12 @@
 #!/bin/sh
 
+export PYTHONPATH=/home/src/extinctionr/
+
+source /home/xr/venv/bin/activate
+
 export DEBUG='false'
 export DJANGO_SETTINGS_MODULE=extinctionr.prod_settings
 
-python3.7 manage.py migrate
-python3.7 manage.py collectstatic --noinput
+python manage.py migrate
+python manage.py collectstatic --noinput
 exec gunicorn extinctionr.wsgi
