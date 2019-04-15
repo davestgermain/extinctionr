af53be5153c9ce9992633493d6d9c856bcdd9e50
Dave St.Germain <dcs@adullmoment.com>
2019-04-14 22:50:57 -0400

    green on home page. white elsewhere

--- run.sh
+++ run.sh
@@ -7,6 +7,6 @@ source /home/xr/venv/bin/activate
 export DEBUG='false'
 export DJANGO_SETTINGS_MODULE=extinctionr.prod_settings
 
-python manage.py migrate
-python manage.py collectstatic --noinput
+python manage.py migrate --settings=extinctionr.prod_settings
+python manage.py collectstatic --settings=extinctionr.prod_settings --noinput
 exec gunicorn -w 2 --keep-alive 3600 extinctionr.wsgi
