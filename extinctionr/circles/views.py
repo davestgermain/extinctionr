e9fd8d64412afa6061d8e8999f3603c4bf1c8efd
Dave St.Germain <dcs@adullmoment.com>
2019-04-29 08:03:24 -0400

    caching

--- extinctionr/circles/views.py
+++ extinctionr/circles/views.py
@@ -1,34 +1,55 @@
 from django.shortcuts import render, get_object_or_404, redirect
 from django.contrib.auth.decorators import login_required
+from django.http import HttpResponseForbidden
 from django.views import generic
 from .models import Circle, Contact
 
 
 @login_required
 def add_member(request, pk):
-	circle = get_object_or_404(Circle, pk=pk)
-	email = request.POST['email'].lower()
-	name = request.POST['name']
-	circle.add_member(email, name)
-	return redirect(circle.get_absolute_url())
+    circle = get_object_or_404(Circle, pk=pk)
+    email = request.POST['email'].lower()
+    name = request.POST['name']
+    circle.add_member(email, name)
+    return redirect(circle.get_absolute_url())
 
 
 @login_required
 def person_view(request, contact_id):
-	contact = get_object_or_404(Contact, pk=contact_id)
-	ctx = {'contact': contact}
-	return render(request, 'circles/person.html', ctx)
+    contact = get_object_or_404(Contact, pk=contact_id)
+    ctx = {'contact': contact}
+    return render(request, 'circles/person.html', ctx)
 
 
 class CircleView(generic.DetailView):
-	template_name = 'circles/circle.html'
-	def get_queryset(self):
-		return Circle.objects.select_related('parent').prefetch_related('leads', 'members')
+    template_name = 'circles/circle.html'
+    def get_queryset(self):
+        return Circle.objects.select_related('parent').prefetch_related('leads', 'members')
+
+    def dispatch(self, request, *args, **kwargs):
+        if not request.user.has_perm('circles.view_circle'):
+            return HttpResponseForbidden("you don't have access to see this")
+        return super().dispatch(request, *args, **kwargs)
+
+    def render_to_response(self, context, **response_kwargs):
+        response = super().render_to_response(context, **response_kwargs)
+        response['Cache-Control'] = 'private'
+        return response
 
 
 class TopLevelView(generic.ListView):
-	template_name = 'circles/outer.html'
+    template_name = 'circles/outer.html'
 
-	def get_queryset(self):
-		return 	Circle.objects.filter(parent__isnull=True).prefetch_related('leads', 'members')
+    def get_queryset(self):
+        return  Circle.objects.filter(parent__isnull=True).prefetch_related('leads', 'members').order_by('name')
 
+    @login_required
+    def dispatch(self, request, *args, **kwargs):
+        if not request.user.has_perm('circles.view_circle'):
+            return HttpResponseForbidden("you don't have access to see this")
+        return super().dispatch(request, *args, **kwargs)
+
+    def render_to_response(self, context, **response_kwargs):
+        response = super().render_to_response(context, **response_kwargs)
+        response['Cache-Control'] = 'private'
+        return response
