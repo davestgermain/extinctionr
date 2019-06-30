072a5f7e6499eaa273bc7d28e78cae61490f7a07
Dave St.Germain <dcs@adullmoment.com>
2019-06-30 18:15:08 -0400

    switch from processes to threads

--- run.sh
+++ run.sh
@@ -9,4 +9,4 @@ export DJANGO_SETTINGS_MODULE=extinctionr.prod_settings
 
 python manage.py migrate --settings=extinctionr.prod_settings
 python manage.py collectstatic --settings=extinctionr.prod_settings --noinput
-exec gunicorn -w 2 --keep-alive 3600 extinctionr.wsgi
+exec gunicorn --threads 4 --max-requests 1000 --keep-alive 3600 extinctionr.wsgi
