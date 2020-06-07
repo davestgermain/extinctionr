be9225db8c339ae6d18dd3ef1e40d603e380c036
John B <johnb.xr@protonmail.com>
2020-06-07 06:20:35 -0400

    runs compression step

--- run.sh
+++ run.sh
@@ -9,4 +9,5 @@ export DJANGO_SETTINGS_MODULE=extinctionr.prod_settings
 
 python manage.py migrate --settings=extinctionr.prod_settings
 python manage.py collectstatic --settings=extinctionr.prod_settings --noinput
+python manage.py compress --force
 exec gunicorn --threads 4 --max-requests 1000 --keep-alive 3600 extinctionr.wsgi
