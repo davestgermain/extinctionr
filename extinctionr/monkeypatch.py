edb918ddb56478bb088720eb10d86a431ea9d7e1
John B <johnb.xr@protonmail.com>
2020-06-15 07:39:04 -0400

    monkepath workaround indecipherable xml bug

--- extinctionr/monkeypatch.py
+++ extinctionr/monkeypatch.py
@@ -14,6 +14,7 @@ def do_monkeypatch():
     from common.models import User
     from taggit.managers import TaggableManager
     from wiki.plugins.images.wiki_plugin import ImagePlugin
+    from wiki.core.markdown.mdx.responsivetable import ResponsiveTableTree
 
     # give users an absolute url
     User.get_absolute_url = get_absolute_url
@@ -28,10 +29,25 @@ def do_monkeypatch():
     # overriding the default admin to add some features
     @admin.register(Contact)
     class ContactAdmin(admin.ModelAdmin):
-        search_fields = ('first_name', 'last_name', 'email', 'postcode')
+        search_fields = ('first_name', 'last_name', 'email', 'address__postcode')
         list_display = ('email', 'first_name', 'last_name', 'phone', 'created_on')
         list_filter = ('tags', 'address__city')
         list_select_related = ('address', )
 
     # fix the icon on the image macro
     ImagePlugin.sidebar['icon_class'] = 'fa-images'
+
+    # Fix werid bug in wiki
+    # TODO: revisit this on future upgrades of wiki, markdown, etc.
+    original_responsive_table_tree_run = ResponsiveTableTree.run
+
+    def patched_responsive_table_tree_run(self, root):
+        class ElementWrapper:
+            def __init__(self, root):
+                self.root = root
+
+            def getiterator(self, tag=None):
+                return self.root.findall(tag)
+        original_responsive_table_tree_run(self, ElementWrapper(root))
+        return root
+    ResponsiveTableTree.run = patched_responsive_table_tree_run
